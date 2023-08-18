7957d106e6b81afbf5c2ed737ec01b599dd3abbf
MY_PC <jaysriva18@gmail.com>
2023-08-18 14:28:34 +0530

    commit main

--- app/src/main/java/com/example/jmedia/MainActivity.java
+++ app/src/main/java/com/example/jmedia/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +18,14 @@ import android.widget.Button;
 import com.example.jmedia.Models.NewsApiResponse;
 import com.example.jmedia.Models.NewsHeadlines;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements  SelectListener , View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener {
 
     RecyclerView recyclerView;
     CustomAdapter adapter;
@@ -29,33 +33,37 @@ public class MainActivity extends AppCompatActivity implements  SelectListener ,
 
     String category;
 
-    Button b1,b2,b3,b4,b5,b6,b7;
+    Button b1, b2, b3, b4, b5, b6, b7;
     SearchView searchView;
+    boolean   flag = false;
+
+    ArrayList<Button>buttonArrayList;
 
 
-    List<NewsHeadlines>data;
+    List<NewsHeadlines> data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView=findViewById(R.id.recycler_main);
-        b1=findViewById(R.id.btn_1);
-        b2=findViewById(R.id.btn_2);
-        b3=findViewById(R.id.btn_3);
-        b4=findViewById(R.id.btn_4);
-        b5=findViewById(R.id.btn_5);
-        b6=findViewById(R.id.btn_6);
-        b7=findViewById(R.id.btn_7);
-        searchView=findViewById(R.id.search_view);
-
-
-        b1.setOnClickListener(this);
-        b2.setOnClickListener(this);
-        b3.setOnClickListener(this);
-        b4.setOnClickListener(this);
-        b5.setOnClickListener(this);
-        b6.setOnClickListener(this);
-        b7 .setOnClickListener(this);
+        recyclerView = findViewById(R.id.recycler_main);
+        b1 = findViewById(R.id.btn_1);
+        b2 = findViewById(R.id.btn_2);
+        b3 = findViewById(R.id.btn_3);
+        b4 = findViewById(R.id.btn_4);
+        b5 = findViewById(R.id.btn_5);
+        b6 = findViewById(R.id.btn_6);
+        b7 = findViewById(R.id.btn_7);
+        searchView = findViewById(R.id.search_view);
+        buttonArrayList= new ArrayList<>(7);
+        //Adding buttons in buttonArrayList;
+        buttonArrayList.add(b1);
+        buttonArrayList.add(b2);
+        buttonArrayList.add(b3);
+        buttonArrayList.add(b4);
+        buttonArrayList.add(b5);
+        buttonArrayList.add(b6);
+        buttonArrayList.add(b7);
 
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -63,11 +71,11 @@ public class MainActivity extends AppCompatActivity implements  SelectListener ,
             public boolean onQueryTextSubmit(String query) {
                 dialog.setTitle("Fetching news artcles of " + query);
                 dialog.show();
-                RetrofitInstance.getInstance().callNewsApi.callHeadlines("us","general",query,getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
+                RetrofitInstance.getInstance().callNewsApi.callHeadlines("us", "general", query, getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
                     @Override
                     public void onResponse(Call<NewsApiResponse> call, Response<NewsApiResponse> response) {
                         dialog.dismiss();
-                        data= response.body().getArticles();
+                        data = response.body().getArticles();
                         showNews(data);
                     }
 
@@ -87,15 +95,15 @@ public class MainActivity extends AppCompatActivity implements  SelectListener ,
             }
         });
 
-        dialog= new ProgressDialog(this);
+        dialog = new ProgressDialog(this);
         dialog.setTitle("Fetching news Articles");
         dialog.show();
 
-        RetrofitInstance.getInstance().callNewsApi.callHeadlines("in","general",null,getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
+        RetrofitInstance.getInstance().callNewsApi.callHeadlines("in", "general", null, getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
             @Override
             public void onResponse(Call<NewsApiResponse> call, Response<NewsApiResponse> response) {
 
-                data= response.body().getArticles();
+                data = response.body().getArticles();
                 showNews(data);
             }
 
@@ -108,27 +116,34 @@ public class MainActivity extends AppCompatActivity implements  SelectListener ,
 
 
 
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+
+
 
 
     }
 
 
-
-
     private void showNews(List<NewsHeadlines> list) {
 
         dialog.dismiss();
-        recyclerView= findViewById(R.id.recycler_main);
+        recyclerView = findViewById(R.id.recycler_main);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager( new GridLayoutManager(this,1));
-        adapter = new CustomAdapter(this,list,this);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new CustomAdapter(this, list, this);
         recyclerView.setAdapter(adapter);
     }
 
     @Override
     public void OnNewsClicked(NewsHeadlines headlines) {
 
-            startActivity(new Intent(MainActivity.this, DetailsActivity.class).putExtra("data",headlines));
+        startActivity(new Intent(MainActivity.this, DetailsActivity.class).putExtra("data", headlines));
 
 
     }
@@ -136,31 +151,37 @@ public class MainActivity extends AppCompatActivity implements  SelectListener ,
 
     @Override
     public void onClick(View v) {
-        Button button =(Button) v;
-         category=  button.getText().toString();
-         String prev_category=category;
-        dialog.setTitle("Fetching news article of "+ category);
-        button.setBackgroundColor(getResources().getColor(R.color.yellowbutton));
-        dialog.show();
+        Button button = (Button) v;
+        int id=button.getId();
+        category = button.getText().toString();
+        //The logic I have added here is that on clicking the button the background of that button changes to some other color while the rest keep their o
+        for(int i=0; i< buttonArrayList.size();i++){
+            if(buttonArrayList.get(i)== button){
+                buttonArrayList.get(i).setBackgroundColor(getColor(R.color.redbg));
+            }else {
+                buttonArrayList.get(i).setBackgroundColor(getColor(R.color.greenbg));
+            }
+        }
 
-        RetrofitInstance.getInstance().callNewsApi.callHeadlines("us",category,null,getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
+        dialog.setTitle("Fetching news article of " + category);
+        dialog.show();
+        RetrofitInstance.getInstance().callNewsApi.callHeadlines("us", category, null, getApplication().getString(R.string.API_KEY)).enqueue(new Callback<NewsApiResponse>() {
             @Override
             public void onResponse(Call<NewsApiResponse> call, Response<NewsApiResponse> response) {
                 dialog.dismiss();
-                data= response.body().getArticles();
+                data = response.body().getArticles();
                 showNews(data);
-
-                if(category!=prev_category)
-                    button.setBackgroundColor(getResources().getColor(R.color.greenbg));
             }
 
             @Override
             public void onFailure(Call<NewsApiResponse> call, Throwable t) {
 
             }
+
         });
 
 
-
     }
+
+
 }
